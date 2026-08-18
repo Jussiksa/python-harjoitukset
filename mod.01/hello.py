0dfa1d226c0ce5f5dac79db66265fcba6e680deb
Jussi Saarela <jussiksa@metropolia.fi>
2026-08-18 16:02:07 +0300

    jussiksa@metropolia.fi

---
+++ mod.01/hello.py
@@ -0,0 +1,14 @@
+print ("hei maailma")
+print ("mikä sinun nimesi on?")
+name input ("minun nimeni on Jussi")
+print ("terve , Jussi")
+# Ohjelmisto 1 - Python harjoitukset
+
+**Jussi Saarela**
+
+## Moduuli 1
+
+Tein tehtävät 1 ja 2.
+
+
+
