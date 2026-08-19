2dd9096e4652cc55a970bc841b0d6f84053e52da
Jussi Saarela <jussiksa@metropolia.fi>
2026-08-19 11:41:53 +0300

    Lisätty mod03 ja korjattu virheet

--- mod.01/hello.py
+++ mod.01/hello.py
@@ -1,14 +1,11 @@
 print ("hei maailma")
 print ("mikä sinun nimesi on?")
-name input ("minun nimeni on Jussi")
-print ("terve , Jussi")
+name=input ("Jussi Saarela" )
+print ("terve , Jussi Saarela")
 # Ohjelmisto 1 - Python harjoitukset
-
-**Jussi Saarela**
-
-## Moduuli 1
-
-Tein tehtävät 1 ja 2.
+#**Jussi Saarela**
+# ## Moduuli 1
+#Tein tehtävät 1 ja 2.
 
 
 
---
+++ mod03-esim/mod3-es.py
@@ -0,0 +1,19 @@
+# tuntiesimerkit19.
+teksti = "tämä on laskukone, anna kaksi lukua"
+luku = input("anna 1. luku")
+luku2 = input("anna 2. luku")
+ 
+luku=float(luku)
+luku2=float(luku2) 
+teksti = "tämä on merkkijono"
+#luku = 10
+#luku2 = 12.3
+summa = luku + luku2
+# sama liitosoperaattorilla(+)
+summa = str(summa)
+#print ("summa:"))
+
+print ("summa", summa)
+uusi_Kayttaja = input("anna nimesi")
+print("Hauska tavata, " + uusi_Kayttaja + "!")
+  
